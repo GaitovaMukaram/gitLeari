d34eea2dedddcd2cccc37421915123d7414df2ee
Accordion <gaitovamukaram@gmail.com>
2021-09-01 15:40:46 +0600

    git init

---
+++ GitDemo2/GitDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemo2App.swift
+//  GitDemo2
+//
+//  Created by Narumato Nio on 01.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
